35d8458b497bb7209bf307cd430673d7d1876115
ArleneDcosta <arlenedcosta77@gmail.com>
2019-01-01 22:50:57 +0530

    npm run eject

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 // Radium is default export and StyleRoot is named default
 
 const person = (props) => {
@@ -7,7 +7,7 @@ const person = (props) => {
 	
 	//it will overwrite class setting in the css default format
 	return ( 
-		<div className="Person" >
+		<div className={classes.Person}>
 		<p onClick={props.click}>Im {props.name} and I am {props.age} years old!!</p>
 		<p>{props.children}</p>
 		<input type='text' onChange={props.changed} value={props.name}/>
